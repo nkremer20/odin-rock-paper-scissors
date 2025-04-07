70092c7077573a0e9b546562b6171bdbe1f98028
Noah Kremer <75767608+nkremer20@users.noreply.github.com>
2025-04-06 23:53:56 -0500

    Add human prompt function

--- rockPaperScissors.js
+++ rockPaperScissors.js
@@ -1,7 +1,7 @@
 function getComputerChoice() {
     const random_choice = Math.floor((Math.random() * 3) + 1);
     let rpc = ""
-    console.log(random_choice);
+
     switch(random_choice) {
         case 1:
             rpc = "Rock";
@@ -13,8 +13,15 @@ function getComputerChoice() {
             rpc = "Scissors";
             break;
     }
-    console.log(rpc);
     
-    return rpc
+    return rpc;
 };
 
+function getHumanChoice() {
+    choice = prompt("Type Rock, Paper or, Scissors");
+    return choice;
+}
+
+const computerChoice = getComputerChoice();
+
+const humanChoice = getHumanChoice();
