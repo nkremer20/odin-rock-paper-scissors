923b66cc6c8699f9b9adfe56bf398a88798dc932
Noah Kremer <75767608+nkremer20@users.noreply.github.com>
2025-04-07 23:21:27 -0500

    Add playRound function

--- rockPaperScissors.js
+++ rockPaperScissors.js
@@ -13,15 +13,64 @@ function getComputerChoice() {
             rpc = "Scissors";
             break;
     }
-    
+    console.log(rpc);
     return rpc;
 };
 
 function getHumanChoice() {
     choice = prompt("Type Rock, Paper or, Scissors");
+    console.log(choice);
     return choice;
 }
 
-const computerChoice = getComputerChoice();
+function playRound(humanChoice, computerChoice) {
+    // Make choices lower case, so the values are case insensitive
+    const humanChoiceLower = humanChoice.toLowerCase();
+    const computerChoiceLower = computerChoice.toLowerCase();
 
-const humanChoice = getHumanChoice();
+    switch(humanChoiceLower) {
+        case "rock":
+            if (computerChoiceLower === "paper") {
+                ++computerScore;
+                console.log("You lost! Paper beats Rock");
+            } else if (computerChoiceLower === "scissors") {
+                ++humanScore;
+                console.log("You won! Rock beats Scissors");
+            } else {
+                console.log("Tie!")
+            };
+            break;
+
+        case "paper":
+            if (computerChoiceLower === "scissors") {
+                ++computerScore;
+                console.log("You lost! Scissors beats Paper");
+            } else if (computerChoiceLower === "rock") {
+                ++humanScore;
+                console.log("You won! Paper beats Rock");
+            } else {
+                console.log("Tie!")
+            };
+            break;
+
+        case "scissors":
+            if (computerChoiceLower === "rock") {
+                ++computerScore;
+                console.log("You lost! Rock beats Scissors");
+            } else if (computerChoiceLower === "paper") {
+                ++humanScore;
+                console.log("You won! Scissors beats Paper");
+            } else {
+                console.log("Tie!")
+            };
+            break;
+    }
+}
+
+let humanScore = 0;
+let computerScore = 0;
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection);
